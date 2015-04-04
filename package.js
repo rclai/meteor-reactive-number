5c11c2e5f6ac2571d1a3f87eab998456a2eecada
Richard Lai <it.rclai@gmail.com>
2015-04-03 22:07:17 -0400

    Added Git URL

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Manipulate and fetch a number reactively.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/rclai/meteor-reactive-number.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
