fd27b923ccf8da5076dd0b83926b2df96eeac4b1
Richard Lai <it.rclai@gmail.com>
2015-05-14 21:30:24 -0400

    Bumped to 2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lai:reactive-number',
-  version: '1.0.2',
+  version: '2.0.0',
   // Brief, one-line summary of the package.
   summary: 'Manipulate and fetch a number reactively.',
   // URL to the Git repository containing the source code for this package.
