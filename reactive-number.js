c06a6480e806d78881e506e601ec7427e1aed723
Richard Lai <it.rclai@gmail.com>
2015-05-14 21:29:58 -0400

    Add new features
    Added chaining and added Math functions.

--- reactive-number.js
+++ reactive-number.js
@@ -1,6 +1,6 @@
 ReactiveNumber = function (number) {
   this._isNumberAllowed(number);
-  this._number = number || 0;
+  this._number = number;
   this._dep = new Tracker.Dependency();
 };
 
@@ -26,27 +26,57 @@ ReactiveNumber.prototype.set = function (number) {
   }
   this._number = number;
   this._dep.changed();
+  return this;
 };
 
 ReactiveNumber.prototype.add = function (number) {
-  this.set(this._number + number);  
+  this.set(this._number + number);
+  return this;
 };
 
 ReactiveNumber.prototype.subtract = function (number) {
   this.set(this._number - number);
+  return this;
 };
 
 ReactiveNumber.prototype.multiply = function (number) {
   this.set(this._number * number);
+  return this;
 };
 
 ReactiveNumber.prototype.divide = function (number) {
   this.set(this._number / number);
+  return this;
 };
 
 ReactiveNumber.prototype.calculate = function (fn) {
   if (!_.isFunction(fn)) {
-    throw new Meteor.Error('wrong-argument', 'You must pass a function to `calculate` method of ReactiveNumber.');
+    throw new Meteor.Error('wrong-argument', 'You must pass a function to the `calculate` method of the ReactiveNumber instance.');
   }
   this.set(fn(this._number));
+  return this;
 };
+
+_(_(Math).pick(
+  'abs',
+  'acos',
+  'asin',
+  'atan',
+  'ceil',
+  'cos',
+  'exp',
+  'floor',
+  'log',
+  'round',
+  'sin',
+  'sqrt',
+  'tan',
+  'max',
+  'min',
+  'pow'
+)).each(function (mathFn, fnName) {
+  ReactiveNumber.prototype[fnName] = function () {
+    this.set(mathFn.apply(null, [this._number].concat(Array.prototype.slice.call(arguments))));
+    return this;
+  }
+});
